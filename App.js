2d7df0cbb04cd7c9392c627d158efe692b58c297
Ruchi <jainruchi800@gmail.com>
2023-12-14 10:26:44 +0530

    ep-01

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading=React.createElement("h1",{id:"heading"},[
+    React.createElement("div",{id:"child"},"I am child one"),
+    React.createElement("div",{id:"childTwo"},"I am child two")
+]);
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
